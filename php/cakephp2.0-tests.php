f8fc7653f0c269ac92f2c7c44c265679f38d04ab
AD7six <andydawson76@gmail.com>
2011-04-27 10:24:13 +0200

    make 2.0 hook represent current 2.0 branch code

--- php/cakephp2.0-tests.php
+++ php/cakephp2.0-tests.php
@@ -3,47 +3,76 @@
 
 require dirname(__DIR__) .'/utils.php';
 
-function testFile($file) {
+function testCase($file) {
 	if (!preg_match('@\.php$@', $file) || preg_match('@(config|test_app)[\\\/]@', $file)) {
 		return false;
 	}
+	$category = testCategory($file);
+	$case = str_replace('.php', '', $file);
 	if (preg_match('@tests[\\\/]@', $file)) {
-		if (!preg_match('@\Test\.php$@', $file)) {
-			return false;
+		if (preg_match('@\Test\.php$@', $file)) {
+			if ($case = preg_replace('@.*tests[\\\/]cases[\\\/]@', '', $case)) {
+				$case = str_replace('Test', '', $case);
+				if ($category === 'core') {
+					$case = str_replace('lib' . DS . 'Cake' . DS . 'tests' . DS . 'Case' . DS, '', $case);
+				}
+				return array($category, $case);
+			}
 		}
-		$file = str_replace('Test.php', '.php', $file);
-
-		if (preg_match('@.*lib[\\\/]Cake[\\\/]@', $file)) {
-			return preg_replace('@.*tests[\\\/]cases[\\\/]@', 'lib' . DS . 'Cake' . DS, $file);
-		}
-		return preg_replace('@.*tests[\\\/]cases[\\\/]@', '', $file);
+		return array(false, false);
 	}
-	return $file;
+	if ($category === 'core') {
+		$case = preg_replace('@.*lib[\\\/]Cake[\\\/]@', '', $case);
+		$case[0] = strtoupper($case[0]);
+		return array('core', $case);
+	} else {
+		$relativeFile = preg_replace('@.*((?:(?:config|Console|Controller|Lib|locale|Model|plugins|tests|vendors|View|webroot)[\\\/])|App[-a-z]*$)@', '\1', $case);
+		$baseDir = str_replace($relativeFile, '', $case);
+	}
+
+	return array($category, $relativeFile);
 }
 
+function testCategory($file) {
+	$_file = realpath($file);
+	if ($_file) {
+		$file = $_file;
+	}
+
+	$ds = DIRECTORY_SEPARATOR;
+	if (strpos($file, "{$ds}lib{$ds}Cake{$ds}") !== false) {
+		return 'core';
+	} elseif (preg_match('@plugins[\\\/]([^\\/]*)@', $file, $match)) {
+		return $match[1];
+	}
+	return 'app';
+}
+
+
 $stagedFiles = stagedFiles();
-$filename_pattern = '/\.(php)$/';
-$toTest = array();
+$testCases = array();
 $exit_status = 0;
 
 foreach ($stagedFiles as $file) {
-	$file = testFile($file);
+	list($category, $case) = testCase($file);
 
-	if (!$file) {
+	if (!$case) {
 		continue;
 	}
-	$toTest[$file] = true;
+	$testCases[$category][$case] = true;
 }
 
-foreach($toTest as $file) {
-	$output = array();
-	$cmd = "cake test $file";
-	echo "$cmd\n";
-	exec($cmd, $output, $return);
+foreach($testCases as $category => $cases) {
+	foreach(array_keys($cases) as $case) {
+		$output = array();
+		$cmd = "cake testsuite $category $case";
+		echo "$cmd\n";
+    	exec($cmd, $output, $return);
 
-	if ($return != 0) {
-		echo implode("\n", $output), "\n";
-		$exit_status = 1;
+		if ($return != 0) {
+			echo implode("\n", $output), "\n";
+			$exit_status = 1;
+		}
 	}
 }
 
