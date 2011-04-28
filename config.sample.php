93533609017da6f7f96acf34696052016e5b60b5
AD7six <andydawson76@gmail.com>
2011-04-28 14:34:44 +0200

    adding a comment

--- config.sample.php
+++ config.sample.php
@@ -1,5 +1,18 @@
 <?php
-
+/**
+ * The format for this array is:
+ * 	branchnamepattern
+ * 		git-hook
+ *			script to run
+ * 			OR
+ *			script to run => halt on error
+ *
+ * the branch name pattern is optional, if you want one set of hooks for all branches
+ * you can remove that level from the array.
+ *
+ * Anything else present in the array is ignored by the one-hook script, but can be
+ * picked up by the individual scripts
+ */
 $config = array(
 	'master' => array(),
 	'develop' => array(),
