d4f5d8e16a0a813974d1a7614856ecd6631cf69b
AD7six <andydawson76@gmail.com>
2011-04-28 11:42:17 +0200

    applying commit hooks to phplint
    changing variable names, indentation

--- php/lint.php
+++ php/lint.php
@@ -5,31 +5,32 @@ require dirname(__DIR__) .'/utils.php';
 $stagedFiles = stagedFiles();
 $tmp = copyFiles($stagedFiles);
 
-if ($tmp['dir'] && !is_dir($tmp['dir'])) {
-	echo "{$tmp['dir']} doesn't exist\n";
-	exit(1);
-}
 if (empty($tmp['files'])) {
 	echo "No files to check\n";
 	exit(0);
 }
-
-$filename_pattern = '/\.php$/';
-$exit_status = 0;
-
-foreach ($tmp['files'] as $file) {
-    if (!preg_match($filename_pattern, $file)) {
-        continue;
-    }
-
-	$cmd = "php -l " . escapeshellarg($file);
-    $lint_output = array();
-	echo "$cmd\n";
-    exec($cmd, $lint_output, $return);
-    if ($return != 0) {
-        echo implode("\n", $lint_output), "\n";
-        $exit_status = 1;
-    }
+if ($tmp['dir'] && !is_dir($tmp['dir'])) {
+	echo "{$tmp['dir']} doesn't exist\n";
+	exit(1);
 }
 
-exit($exit_status);
+
+$pattern = '/\.php$/';
+$status = 0;
+
+foreach ($tmp['files'] as $file) {
+	if (!preg_match($pattern, $file)) {
+		continue;
+	}
+
+	$cmd = "php -l " . escapeshellarg($file);
+	$output = array();
+	echo "$cmd\n";
+	exec($cmd, $output, $return);
+	if ($return != 0) {
+		echo implode("\n", $output), "\n";
+		$status = 1;
+	}
+}
+
+exit($status);
