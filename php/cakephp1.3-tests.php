0659cbad3367faea532768dd0533dd4efe237c37
AD7six <andydawson76@gmail.com>
2011-05-07 21:21:30 +0200

    update 1.3 test runner

--- php/cakephp1.3-tests.php
+++ php/cakephp1.3-tests.php
@@ -3,36 +3,76 @@
 
 require dirname(__DIR__) .'/utils.php';
 
+/**
+ * testCase
+ *
+ * Find the test case for the passed file. The file could itself be a test.
+ *
+ * @param mixed $file
+ * @access public
+ * @return array(type, case)
+ */
 function testCase($file) {
 	if (!preg_match('@\.php$@', $file) || preg_match('@(config|test_app)[\\\/]@', $file)) {
 		return false;
 	}
-	$category = testCategory($file);
-	$case = str_replace('.php', '', $file);
+
+	$return = array(
+		'category' => testCategory($file),
+		'case' => str_replace('.php', '', $file),
+		'testFile' => false,
+		'testFileExists' => false,
+	);
+
 	if (preg_match('@tests[\\\/]@', $file)) {
-		if (preg_match('@\Test\.php$@', $file)) {
-			if ($case = preg_replace('@.*tests[\\\/]cases[\\\/]@', '', $case)) {
-				$case = str_replace('Test', '', $case);
-				if ($category === 'core') {
-					$case = str_replace('lib' . DS . 'Cake' . DS . 'tests' . DS . 'Case' . DS, '', $case);
+		if (preg_match('@.test\.php$@', $file)) {
+			$return['testFile'] = $file;
+			$return['testFileExists'] = true;
+			if ($return['case'] = preg_replace('@.*tests[\\\/]cases[\\\/]@', '', $return['case'])) {
+				$return['case'] = str_replace('.test', '', $return['case']);
+				if ($return['category'] === 'core') {
+					$return['case'] = str_replace('lib' . DS, '', $return['case']);
 				}
-				return array($category, $case);
 			}
 		}
-		return array(false, false);
-	}
-	if ($category === 'core') {
-		$case = preg_replace('@.*lib[\\\/]Cake[\\\/]@', '', $case);
-		$case[0] = strtoupper($case[0]);
-		return array('core', $case);
+	} elseif ($return['category'] === 'core') {
+		$return['testFile'] = preg_replace('@(.*cake[\\\/])@', '\1' . 'tests' . DS . 'cases' . DS, $return['case']) . '.test.php';
+
+		$return['case'] = preg_replace('@.*cake[\\\/]?(libs[\\\/])?@', '', $return['case']);
+		$return['case'] = str_replace('libs' . DS, '', $return['case']);
 	} else {
-		$relativeFile = preg_replace('@.*((?:(?:config|Console|Controller|Lib|locale|Model|plugins|tests|vendors|View|webroot)[\\\/])|App[-a-z]*$)@', '\1', $case);
-		$baseDir = str_replace($relativeFile, '', $case);
+		$return['case'] = preg_replace(
+			'@(?:controllers[\\\/](components[\\\/])|models[\\\/](behaviors|datasources[\\\/])|views[\\\/](helpers[\\\/])|vendors[\\\/](shells[\\\/]))@',
+			'\1',
+			$return['case']
+		);
+		$return['testFile'] = preg_replace(
+			'@(.*)((?:(?:config|controllers|componentslibs|locale|models|behaviors|datasources|helpers|shells|plugins|tests|vendors|views)[\\\/]).*$|app[-a-z]*$)@',
+			'\1tests/cases/\2\.test.php',
+			$return['case']
+		);
+
+		$return['case'] = preg_replace(
+			'@.*((?:(?:config|controllers|componentslibs|locale|models|behaviors|datasources|helpers|shells|plugins|tests|vendors|views)[\\\/]).*$|app[-a-z]*$)@',
+			'\1',
+			$return['case']
+		);
+
 	}
 
-	return array($category, $relativeFile);
+	$return['testFileExists'] = file_exists($return['testFile']);
+	return $return;
 }
 
+/**
+ * testCategory
+ *
+ * For the given file, what category of test is it? returns app, core or the name of the plugin
+ *
+ * @param mixed $file
+ * @access public
+ * @return string
+ */
 function testCategory($file) {
 	$_file = realpath($file);
 	if ($_file) {
@@ -40,7 +80,7 @@ function testCategory($file) {
 	}
 
 	$ds = DIRECTORY_SEPARATOR;
-	if (strpos($file, "{$ds}lib{$ds}Cake{$ds}") !== false) {
+	if (strpos($file, "{$ds}cake{$ds}") !== false) {
 		return 'core';
 	} elseif (preg_match('@plugins[\\\/]([^\\/]*)@', $file, $match)) {
 		return $match[1];
@@ -48,32 +88,75 @@ function testCategory($file) {
 	return 'app';
 }
 
+function testCases($files = null) {
+	$return = array();
 
-$files = files();
-$testCases = array();
-$exit_status = 0;
-
-foreach ($files as $file) {
-	list($category, $case) = testCase($file);
-
-	if (!$case) {
-		continue;
+	if (is_null($files)) {
+		$files = files();
 	}
-	$testCases[$category][$case] = true;
+	foreach ($files as $file) {
+		$data = testCase($file);
+
+		if ($data === false || !$data['testFile']) {
+			continue;
+		}
+		if (!$data['testFileExists']) {
+			echo "Skipping $file (test case {$data['testFile']} not found)\n";
+			continue;
+		}
+		$return[$data['category']][$data['case']] = true;
+	}
+	return $return;
 }
 
-foreach($testCases as $category => $cases) {
-	foreach(array_keys($cases) as $case) {
-		$output = array();
-		$cmd = "cake testsuite $category $case";
-		echo "$cmd\n";
-    	exec($cmd, $output, $return);
-
-		if ($return != 0) {
-			echo implode("\n", $output), "\n";
-			$exit_status = 1;
+function runTestCases($files = null) {
+	$exit = 0;
+	foreach(testCases($files) as $category => $cases) {
+		foreach(array_keys($cases) as $case) {
+			$output = array();
+			$cmd = "cake testsuite $category case $case";
+			echo "$cmd ... \t";
+			exec($cmd, $output, $return);
+			if ($return != 0) {
+				echo "\n" . implode("\n\t", $output), "\n";
+				$exit = 1;
+			} else {
+				echo "OK\n";
+			}
 		}
 	}
+	return $exit;
 }
 
-exit($exit_status);
+function writeTest() {
+	$out = "<?php\ndefine('RUNNING_TESTS', true);\nrequire __DIR__ . '/cakephp2.0-tests.php';\n\n";
+	$out .= "class Cakephp20HookTest extends PHPUnit_Framework_TestCase {\n\n\tfunction testTestCase() {\n";
+	foreach (files() as $file) {
+		$data = testCase($file);
+
+		if (!$data) {
+			continue;
+		}
+
+		$out .=  "\n\t\t\$result = testCase('$file');\n";
+		if (!$data['testFile']) {
+			$out .=  "\t\t\$this->assertFalse(\$result['testFile']);\n";
+			continue;
+		}
+		$out .=  "\t\t\$this->assertEquals('{$data['category']}', \$result['category']);\n";
+		$out .=  "\t\t\$this->assertEquals('{$data['case']}', \$result['case']);\n";
+		$out .=  "\t\t\$this->assertEquals('{$data['testFile']}', \$result['testFile']);\n";
+	}
+
+	$out .= "\t}\n}";
+	file_put_contents('cakephp2.0-tests.test.php', $out);
+	echo $out;
+}
+
+if (defined('WRITE_TESTS')) {
+	writeTest();
+} elseif (!defined('RUNNING_TESTS')) {
+	$exit = runTestCases();
+	exit($exit);
+}
+
