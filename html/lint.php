4d9c59ac3ee7cfada9fb0be4d8426d1a01798d99
AD7six <andydawson76@gmail.com>
2011-05-12 10:27:04 +0200

    remove unused var

--- html/lint.php
+++ html/lint.php
@@ -3,8 +3,6 @@
 
 require $_SERVER['PWD'] . '/.git/hooks/utils.php';
 
-$config = config();
-
 $files = files();
 $tmp = copyFiles($files);
 
@@ -16,8 +14,6 @@ if ($tmp['dir'] && !is_dir($tmp['dir'])) {
 	echo "{$tmp['dir']} doesn't exist\n";
 	exit(1);
 }
-
-$config = $config['js']['lint'];
 
 $status = 0;
 
