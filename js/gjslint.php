1508adb671539235e27cf799f704914109bf829b
AD7six <andydawson76@gmail.com>
2011-05-26 18:50:48 +0200

    adding a stub for running gjslint
    jslint is pretty awesome, but gjslint has the added advantage that it'll
    correct things for you too.. so give that a go

---
+++ js/gjslint.php
@@ -0,0 +1,39 @@
+#!/usr/bin/env php
+<?php
+
+require $_SERVER['PWD'] . '/.git/hooks/utils.php';
+
+$config = config();
+
+$files = files();
+$tmp = copyFiles($files);
+
+if (empty($tmp['files'])) {
+	echo "No files to check\n";
+	exit(0);
+}
+if ($tmp['dir'] && !is_dir($tmp['dir'])) {
+	echo "{$tmp['dir']} doesn't exist\n";
+	exit(1);
+}
+
+$config = $config['js']['lint'];
+
+$status = 0;
+
+foreach ($tmp['files'] as $file) {
+	if (!preg_match($config['pattern'], $file)) {
+		continue;
+	}
+
+	$cmd = "gjslint " . escapeshellarg($file);
+	$output = array();
+	echo "$cmd\n";
+	exec($cmd, $output, $return);
+	if ($return) {
+		echo implode("\n", $output), "\n";
+		$status = 1;
+	}
+}
+
+exit($status);
